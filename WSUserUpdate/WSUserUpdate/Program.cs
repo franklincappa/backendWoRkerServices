6706363ad7e68a41df427881aca61b9b0b5dd321
Franklin Cappa Ticona <franklincappa@gmail.com>
2024-03-24 07:56:32 -0500

    Configurar Serilog

--- WSUserUpdate/WSUserUpdate/Program.cs
+++ WSUserUpdate/WSUserUpdate/Program.cs
@@ -1,11 +1,21 @@
 using WSUserUpdate;
-
+using Serilog;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         //services.AddHostedService<Worker>();
         services.AddHostedService<UserService>();
     })
+    .UseSerilog()
     .Build();
 
+var configSetting = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json").Build();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File(configSetting["Logging:LogPath"])
+    .CreateLogger();
 host.Run();
